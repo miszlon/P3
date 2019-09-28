a46263a949631907b65badeba02ad92cab3ab504
miszlon <milosz.szkudlarz@gmail.com>
2019-09-28 13:44:26 +0200

    Lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string odpowiedz = Console.ReadLine();
+            int liczba = Convert.ToInt32(odpowiedz);
+
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine("Parzysta");
+            }
+            else Console.WriteLine("Nieparzysta");
+
+            Console.Read();
+        }
+    }
+}
+
