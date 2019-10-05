08ade09c360c4fa2fe32cda0138651b76d2ccd5a
miszlon <milosz.szkudlarz@gmail.com>
2019-10-05 14:41:44 +0200

    Laboratoria 3

---
+++ Lab3.1/Lab3.1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3._1
+{
+    class Funkcja1
+    {
+        public float ObliczSrednia(float suma, float iloscElementow)
+        {
+            return (float)suma / iloscElementow;
+        }
+    }
+}
--- Lab3.1/Lab3.1/Program.cs
+++ Lab3.1/Lab3.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Lab3._1
 {
@@ -6,7 +7,40 @@ namespace Lab3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Laboratoria 3");
+            // obliczanie średniej ucznia, jeden przedmiot i nie wiadomi ile ocen
+            
+
+            StringBuilder builder = new StringBuilder();
+
+            string text = "@";
+            float sum = 0;
+            float result = 0;
+            Funkcja1 obiekt = new Funkcja1();
+
+            while (text != string.Empty)
+            {
+                text = Console.ReadLine();
+                builder.Append(text + ",");
+            }
+            string gradesTring = builder.ToString();
+            string[] grades = gradesTring.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            float[] gradesInt = new float[grades.Length];
+
+            for (int i = 0; i < gradesInt.Length; i++)
+            {
+                gradesInt[i] = Convert.ToSingle(grades[i]);
+            }
+
+            foreach(var grade in gradesInt)
+            {
+                sum += grade;
+            }
+
+            result = obiekt.ObliczSrednia(sum, gradesInt.Length);
+            Console.WriteLine(result);
+
+   
         }
     }
 }
