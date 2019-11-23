3bacab40f6cc88e9d8ef43460f9dc9a9f81d46b8
miszlon <milosz.szkudlarz@gmail.com>
2019-11-23 14:30:12 +0100

    Lab6

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RandomDataGenerator.Randomizers;
+using RandomDataGenerator.FieldOptions;
+
+namespace Lab6
+{
+    class Osoba
+    {
+        public int id;
+        public string imie;
+        public string nazwisko;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = Enumerable.Range(100, 150).ToList();
+            List<int> podzielnePrzez3 = lista.Where(x => x % 3 == 0).ToList();
+
+            int elementyNaStronie = 25;
+            int nrStrony = 2;
+            List<int> strona = lista.Skip(elementyNaStronie * (nrStrony - 1)).Take(elementyNaStronie).ToList();
+
+            double srednia = lista.Average();
+            Console.WriteLine(srednia);
+
+            //foreach (var item in strona)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            List<Osoba> osoby = Enumerable.Range(1, 50).Select(x => new Osoba() { id = x, imie = x.ToString(), nazwisko = "Milosz" }).ToList();
+            
+            foreach (var item in osoby)
+            {
+                Console.WriteLine($"{item.id}: {item.imie}, {item.nazwisko}");
+            }
+
+
+
+            var intGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsInteger());
+            var nameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
+            var lastGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsLastName());
+            Osoba random = new Osoba(
+                intGenerator.Generate().Value, nameGenerator.Generate(), lastGenerator.Generate());
+            Console.WriteLine($"{random.id}: {random.imie} {random.nazwisko}");
+
+            
+
+        }
+    }
+}
+
+    
