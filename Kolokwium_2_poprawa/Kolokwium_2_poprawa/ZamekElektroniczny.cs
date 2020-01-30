f3f3e86d300b79f3dcda3e54f980cf5b0b5ce2d1
miszlon <milosz.szkudlarz@gmail.com>
2020-01-30 18:55:15 +0100

    Kolokwium 2 poprawa

--- Kolokwium_2_poprawa/Kolokwium_2_poprawa/ZamekElektroniczny.cs
+++ Kolokwium_2_poprawa/Kolokwium_2_poprawa/ZamekElektroniczny.cs
@@ -12,11 +12,17 @@ namespace Kolokwium_2_poprawa
             Console.WriteLine("Proszę przyłożyć kartę magnetyczną");
             Console.WriteLine("1 - przyłożenie, 0 - nieprzyłożył karty");
             int karta = int.Parse(Console.ReadLine());
-            if(karta ==1)
+            int licznikWejsc = 0;
+            while (licznikWejsc < 2)
             {
-                Console.WriteLine(data);
+                if (karta == 1)
+                {
+                    Console.WriteLine(data);
+                    licznikWejsc++;
+                }
+                else Console.WriteLine("nie weszłeś!");
             }
-            else Console.WriteLine("nie weszłeś!");
+            Console.WriteLine("Za dużo prób wejść");
         }
         public void Otworz()
         {
