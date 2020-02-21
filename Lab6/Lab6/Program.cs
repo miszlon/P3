2f1c6f1f17aac0c1cf15801c0c7c696ae818ff01
miszlon <55624060+miszlon@users.noreply.github.com>
2020-02-21 20:52:20 +0100

    Poprawione
    Sortowanie dodane

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using RandomDataGenerator.Randomizers;
 using RandomDataGenerator.FieldOptions;
 
-namespace Lab_6 //zjazd 6
+namespace labVI
 {
     class Osoba
     {
-        public int Id;
+        public int id;
         public string imie;
         public string nazwisko;
 
-        public Osoba(int Id, string imie, string nazwisko)
+        public Osoba(int id, string imie, string nazwisko)
         {
-            this.Id = Id;
+            this.id = id;
             this.imie = imie;
             this.nazwisko = nazwisko;
         }
@@ -23,65 +23,58 @@ namespace Lab_6 //zjazd 6
     {
         static void Main(string[] args)
         {
-
+            /*
             List<int> lista = Enumerable.Range(100, 150).ToList();
-            List<int> podzielnaPrzez3 =
-                lista.Where(x => x % 3 == 0).ToList();
-            int elementynaStronie = 25;
-            int nrStrony = 2;
-            List<int> strona = lista.Skip(elementynaStronie * (nrStrony - 1)).Take(elementynaStronie).ToList();
+            List<int> podzielnePrzez3 =
+                lista.Where(x => (x % 3 == 0)).ToList();
 
+            int elementyNaStronie = 25;
+            int numerStrony = 2;
+            List<int> strona2 =
+                lista.Skip(elementyNaStronie * (numerStrony - 1)).Take(elementyNaStronie).ToList();
+
+            
             double srednia = lista.Average();
+
             Console.WriteLine(srednia);
 
-            foreach( var item in strona) //listy
-            {
+            foreach (int item in strona2)
                 Console.WriteLine(item);
-            }
+                
+
             List<Osoba> osoby = Enumerable.Range(1, 50)
-                .Select(x => new Osoba()
-                {
-                    Id = x,
-                    imie = x.ToString(),
-                    nazwisko = "aaa"
-
+                .Select(x => new Osoba() { 
+                    id = x, 
+                    imie = x.ToString(), 
+                    nazwisko = "aaa" 
                 }).ToList();
-            osoby[30].nazwisko = "bbb";
 
-            ~~foreach (var item in osoby)
-                Console.WriteLine($"{item.Id}: {item.imie}:{item.nazwisko}");
+            foreach (var item in osoby)
+                Console.WriteLine($"{item.id}: {item.imie} {item.nazwisko} ");
 
-            IEnumerable<int> ids = osoby.Select(x => x.Id);
+            IEnumerable<int> ids = osoby.Select(x => x.id);
 
-            //foreach (var item in ids)
-            //Console.WriteLine(item);
-
-            osoby.Select(x => x.nazwisko).Distinct()
-                .ToList().ForEach(Console.WriteLine);
-            foreach (var item in nazwiska)
+            foreach (var item in ids)
                 Console.WriteLine(item);
 
-            int pierwszy = lista.FirstOrDefault(x => x%300 == 0 );
+            osoby.Select(x => x.nazwisko).Distinct().ToList().ForEach(x => Console.WriteLine(x));
+
+            int pierwszy = lista.FirstOrDefault(x => x % 300 == 0);
+
             Console.WriteLine(pierwszy);
+            */
             var intGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsInteger());
             var nameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
             var lastGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsLastName());
 
-            Osoba random = new Osoba(
-                intGenerator.Generate().Value,
-                nameGenerator.Generate(),
-                lastGenerator.Generate());
-            Console.WriteLine($"{random.Id}:{random.imie}:{random.nazwisko}");
-            //lab 6
+            //Osoba random = new Osoba(intGenerator.Generate().Value, nameGenerator.Generate(), lastGenerator.Generate());
+            //Console.WriteLine($"{random.id}: {random.imie} {random.nazwisko} ");
+
             List<Osoba> osoby = Enumerable.Range(1, 100)
-                .Select(x => new Osoba(intGenerator.Generate().Value, nameGenerator.Generate(), lastGenerator.Generate())).ToList();
+                .Select(x => new Osoba(intGenerator.Generate().Value, nameGenerator.Generate(), nameGenerator.Generate())).ToList();
+                
 
-            foreach (var item in osoby)
-                Console.WriteLine(item.Id + " " + item.imie + " " + item.nazwisko);
-
-
-
+            osoby.OrderBy(x => x.nazwisko).ThenBy(x => x.imie).ToList().ForEach(x => Console.WriteLine($"{x.id}: {x.imie} {x.nazwisko}"));
         }
     }
 }
-//Srednia elementów
